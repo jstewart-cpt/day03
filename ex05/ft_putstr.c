7e3b3b1b351f4e2e344cbc4e6fd9a35c4bec18e4
jstewart3218 <67954951+jstewart3218@users.noreply.github.com>
2020-07-11 14:34:21 +0200

    update to git repo

---
+++ ex05/ft_putstr.c
@@ -0,0 +1,14 @@
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+void ft_putstr(char *str)
+{
+    int inp;
+    inp = 0;
+    while(str[inp] != 0)
+    {
+        ft_putchar(str[inp]);
+        inp ++;
+    }
+}
